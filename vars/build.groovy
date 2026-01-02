04493f7be9431d4d14966fb1a3db4eeb56761b7d
raj01-25 <rajkumarshihi@gmail.com>
2026-01-03 01:36:12 +0530

    Fix docker build command syntax in build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,3 @@
 def call(Map config) {
-    sh """
-       docker build -t ${config.DockerHubUser}/${config.ProjectName}:${config.ImageTag} .
-    """
+    sh """build -t ${config.DockerHubUser}/${config.ProjectName}:${config.ImageTag} ."""
 }
