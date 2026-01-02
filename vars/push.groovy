c78fbc7f767f9881b07f2445c0d616181eb158dc
raj01-25 <rajkumarshihi@gmail.com>
2026-01-03 00:32:11 +0530

    Refactor push function parameters for clarity

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,4 +1,4 @@
-def call(String imageName,  String  ){
+def call( String ProjectName, String ImageTag, String DockerHubUser ){
   withCredentials([usernamePassword('credentialsId': "dockerHubCred",passwordVariable: "dockerHubPass",usernameVariable: "dockerHubUser")])
   {sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass " 
    sh "docker image tag notes-app:latest ${env.dockerHubUser}/notes-app:latest"
